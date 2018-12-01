6009f627aecd2a8865107957ce1eb76b803046c7
Juergen Stuber <juergen@jstuber.net>
2018-12-01 14:40:50 +0100

    Fix predator and prey so that growth is again possible.

--- src/bin/predprey/main.rs
+++ src/bin/predprey/main.rs
@@ -74,7 +74,7 @@ fn grow(board: &Board, pos: Point2d, grow: Square, die: Square) -> Square {
     for dir in Vec2d::directions() {
         let pos1 = pos + dir;
         if board.rect().contains(pos1) {
-            let neigh_sq = board.get(pos);
+            let neigh_sq = board.get(pos1);
             if neigh_sq == grow {
                 return grow;
             }
