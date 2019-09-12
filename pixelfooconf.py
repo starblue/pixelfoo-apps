baf4df6c567594e89f89dc6b338c1f5da162c36f
Juergen Stuber <juergen@jstuber.net>
2019-09-12 22:08:08 +0200

    Add setup script and configuration file for pixelserver2.

---
+++ pixelfooconf.py
@@ -0,0 +1,8 @@
+Apps = [
+    # juergen/pixelfoo
+    {"guiname": "Colored noise", "name": "cnoise", "cmd": "apps/cnoise", "persistent": False},
+    {"guiname": "Dual Moodlight", "name": "bimood", "cmd": "apps/bimood", "persistent": False},
+    {"guiname": "Maze", "name": "maze", "cmd": "apps/maze", "persistent": False},
+    {"guiname": "Dual Maze", "name": "dualmaze", "cmd": "apps/dualmaze", "persistent": False, "persistent": False},
+    {"guiname": "Predator & Prey", "name": "predprey", "cmd": "apps/predprey", "persistent": False},
+]   
