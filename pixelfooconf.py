546bb6cdceed2543c43b63d3834fa58ca962159d
Juergen Stuber <juergen@jstuber.net>
2023-10-22 10:27:48 +0200

    Update pixelserver configuration file

--- pixelfooconf.py
+++ pixelfooconf.py
@@ -1,8 +1,11 @@
 Apps = [
     # juergen/pixelfoo
-    {"guiname": "Colored noise", "name": "cnoise", "cmd": "apps/cnoise", "persistent": False},
-    {"guiname": "Dual Moodlight", "name": "bimood", "cmd": "apps/bimood", "persistent": False},
-    {"guiname": "Maze", "name": "maze", "cmd": "apps/maze", "persistent": False},
-    {"guiname": "Dual Maze", "name": "dualmaze", "cmd": "apps/dualmaze", "persistent": False, "persistent": False},
-    {"guiname": "Predator & Prey", "name": "predprey", "cmd": "apps/predprey", "persistent": False},
+    {"guiname": "Colored noise", "name": "cnoise", "cmd": "pixelfoo/target/release/cnoise", "persistent": False},
+    {"guiname": "Game of Life", "name": "life", "cmd": "pixelfoo/target/release/life", "persistent": False},
+    {"guiname": "Matrix Code", "name": "matrix-code", "cmd": "pixelfoo/target/release/matrix-code", "persistent": False},
+    {"guiname": "Lorenz Attractor", "name": "lorenz", "cmd": "pixelfoo/target/release/lorenz", "persistent": False},
+    {"guiname": "Dual Moodlight", "name": "bimood", "cmd": "pixelfoo/target/release/bimood", "persistent": False},
+    {"guiname": "Maze", "name": "maze", "cmd": "pixelfoo/target/release/maze", "persistent": False},
+    {"guiname": "Dual Maze", "name": "dualmaze", "cmd": "pixelfoo/target/release/dualmaze", "persistent": False, "persistent": False},
+    {"guiname": "Predator & Prey", "name": "predprey", "cmd": "pixelfoo/target/release/predprey", "persistent": False},
 ]   
