65636a0487ce83e88dc5dada68d17625df15b36d
Juergen Stuber <juergen@jstuber.net>
2024-12-31 21:48:54 +0100

    Update configuration fragment

--- pixelfooconf.py
+++ pixelfooconf.py
@@ -1,11 +1,14 @@
 Apps = [
     # juergen/pixelfoo
-    {"guiname": "Congress noise", "name": "cnoise", "cmd": "pixelfoo/target/release/cnoise", "persistent": False},
-    {"guiname": "Game of Life", "name": "life", "cmd": "pixelfoo/target/release/life", "persistent": False},
-    {"guiname": "Matrix Code", "name": "matrix-code", "cmd": "pixelfoo/target/release/matrix-code", "persistent": False},
-    {"guiname": "Lorenz Attractor", "name": "lorenz", "cmd": "pixelfoo/target/release/lorenz", "persistent": False},
-    {"guiname": "Dual Moodlight", "name": "bimood", "cmd": "pixelfoo/target/release/bimood", "persistent": False},
-    {"guiname": "Maze", "name": "maze", "cmd": "pixelfoo/target/release/maze", "persistent": False},
-    {"guiname": "Dual Maze", "name": "dualmaze", "cmd": "pixelfoo/target/release/dualmaze", "persistent": False, "persistent": False},
-    {"guiname": "Predator & Prey", "name": "predprey", "cmd": "pixelfoo/target/release/predprey", "persistent": False},
+    AppConfig(guiname="Congress noise", name="cnoise", cmd="./cnoise", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Color code", name="colorcode", cmd="./colorcode", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Game of Life", name="life", cmd="./life", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Matrix Code", name="matrix-code", cmd="./matrix-code", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Lorenz Attractor", name="lorenz", cmd="./lorenz", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Primes", name="primes", cmd="./primes", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Alien Message", name="alien-message", cmd="./alien-message", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Dual Moodlight", name="bimood", cmd="./bimood", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Maze", name="maze", cmd="./maze", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Dual Maze", name="dualmaze", cmd="./dualmaze", path="pixelfoo-apps/target/release/"),
+    AppConfig(guiname="Predator & Prey", name="predprey", cmd="./predprey", path="pixelfoo-apps/target/release/"),
 ]
